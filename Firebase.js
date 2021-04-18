d7ce597d60dde9619c19dd223309d010835a4437
Ronan Macken <ronan.macken@mydit.ie>
2021-04-18 20:08:39 +0100

    more databasse fucntions added

--- Firebase.js
+++ Firebase.js
@@ -1,4 +1,6 @@
 import Firebase from './FirebaseConfig';
+
+
 export function addToFirestore (object){
 
     Firebase.firestore()
@@ -11,7 +13,15 @@ export function addToFirestore (object){
         paymentMethod:object.paymentMethod}
   ).then((data) => addComplete(data))
   }
-
+  export function addToFirestore2 (collection, object) {
+    Firebase.firestore()
+    .collection(collection)
+    .add({object}) 
+    .then((snapshot)=>{
+        object.id = snapshot.id;
+        snapshot.set(object);
+      })
+  }
 
 deleteFireStore= (type,key)=>{
     Firebase.firestore().collection(type).doc(key).delete()
@@ -32,3 +42,41 @@ export async function getFromFirestore(collection,Retrieved){
     // console.log(Firebase.auth().currentUser.email);
     Retrieved(listType);
     } 
+
+
+    export  async function getItemByType(type,ItemRetrieved){
+    
+      var Items = [];
+    
+    var snapshot = await Firebase.firestore()
+      .collection('Items')
+      .where('Category', '==',type)
+      .get()
+    
+    snapshot.forEach((doc) => {
+      const spot = doc.data();
+      item.id = doc.id;
+      Items.push(spot);
+     
+    })
+    // console.log(Firebase.auth().currentUser.email);
+    ItemRetrieved(Items);
+    }
+    export  async function getItemByType2(type){
+    
+      var Items = [];
+    
+    var snapshot = await Firebase.firestore()
+      .collection('Items')
+      .where('Category', '==',type)
+      .get()
+    
+    snapshot.forEach((doc) => {
+      const spot = doc.data();
+      item.id = doc.id;
+      Items.push(spot);
+     
+    })
+    // console.log(Firebase.auth().currentUser.email);
+    return Items;
+    }
--- FirebaseConfig.js
+++ FirebaseConfig.js
@@ -5,7 +5,9 @@ const firebaseConfig = {
     projectId: "softwarepartternsca",
     storageBucket: "softwarepartternsca.appspot.com",
     messagingSenderId: "775887542139",
-    appId: "1:775887542139:web:1f4c966a41cda437f1cfde"
+    appId: "1:775887542139:web:1f4c966a41cda437f1cfde",
+    databaseURL: "https://softwarepartternsca-default-rtdb.europe-west1.firebasedatabase.app/",
+
 }
 
 let Firebase = firebase.initializeApp(firebaseConfig)
